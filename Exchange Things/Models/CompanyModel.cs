971c39693997822ab31956985134c19de45bb590
Damian Lasoń <lasondamian9@gmail.com>
2020-10-18 13:56:47 +0200

    Added form

---
+++ Exchange Things/Models/CompanyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Exchange_Things.Models
+{
+    public class CompanyModel
+    { 
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+        public bool IsVisible { get; set; } 
+    }
+}
