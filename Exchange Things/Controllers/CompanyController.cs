ac6c1eb0d2e17c0d414c056d7af7e5bd606ee14b
Damian Lasoń <lasondamian9@gmail.com>
2020-10-18 14:35:27 +0200

    Receiving data from a form

--- Exchange Things/Controllers/CompanyController.cs
+++ Exchange Things/Controllers/CompanyController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Exchange_Things.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Exchange_Things.Controllers
 {
     public class CompanyController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible           
+            };
+
+            return View("CompanyAdded", viewModel);            
+
         }
     }
 }
