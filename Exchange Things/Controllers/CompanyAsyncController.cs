9eb60d9944b8e0dfc46efb041affe581e2c71b84
Damian Lasoń <lasondamian9@gmail.com>
2020-11-28 18:50:10 +0100

    lab3 part1

--- Exchange Things/Controllers/CompanyAsyncController.cs
+++ Exchange Things/Controllers/CompanyAsyncController.cs
@@ -4,26 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Exchange_Things.Database;
+using Exchange_Things.Entities;
 using Exchange_Things.Models;
 
-namespace NewBrandingStyle.Web.Controllers
+namespace Exchange_Things.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CompanyAsyncController : ControllerBase
     {
+        private readonly ExchangeThingsContext _context;
+        public CompanyAsyncController(ExchangeThingsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetItems")]
+        public async Task<IActionResult> GetItems()
+        {
+            var items = _context.Items.ToList();
+
+            if (items == null)
+                return NotFound("Nope");
+
+            return Ok(items);
+        }
+
         [HttpPost]
         [Route("AddNewItem")]
         public async Task<IActionResult> AddNewItem(CompanyModel companyModel)
         {
-            var companyAddedModel = new CompanyAddedViewModel
+            var itemEntity = new ItemEntity
             {
-                NumberOfCharsInName = companyModel.Name.Length,
-                NumberOfCharsInDescription = companyModel.Description.Length,
-                IsHidden = !companyModel.IsVisible
+                Name = companyModel.Name,
+                Description = companyModel.Description,
+                IsVisible = companyModel.IsVisible
             };
 
-            return Ok(companyAddedModel);
+            _context.Add(itemEntity);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
---
+++ Exchange Things/Database/ExchangeThingsContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Exchange_Things.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exchange_Things.Database
+{
+    public class ExchangeThingsContext : DbContext
+    {
+        public ExchangeThingsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<ItemEntity> Items { get; set; }
+    }
+}
